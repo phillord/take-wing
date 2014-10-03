a4f32cfee7bef7b1072c5880c8208beabc5b0a06
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-10-03 12:22:35 +0100

    4

---
+++ test/take/wing/getting_started_test.clj
@@ -0,0 +1,6 @@
+(ns take.wing.getting-started-test
+  (:require [take.wing.getting-started :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest getting-started-loaded []
+  (is true))
---
+++ test/take/wing/the_pizza_ontology_test.clj
@@ -0,0 +1,28 @@
+(ns take.wing.the-pizza-ontology-test
+  (:use [clojure.test])
+  (:require [take.wing.the-pizza-ontology :as p]
+            [tawny.owl :as o]
+            [tawny.reasoner :as r]
+            [tawny.fixture]))
+
+(use-fixtures :once
+  (tawny.fixture/namespace-and-reasoner
+   'take.wing.the-pizza-ontology
+   :hermit))
+
+(deftest loading []
+  (is true))
+
+(deftest consistent []
+  (is (r/coherent?))
+  (is (r/consistent?)))
+
+(deftest marg
+  (is (o/subclass? p/Pizza p/MargheritaPizza)))
+
+(deftest pizza-subclasses
+  (is
+   (= #{}
+      (clojure.set/difference
+       #{p/HawaiianPizza p/MargheritaPizza}
+       (o/subclasses p/Pizza)))))
